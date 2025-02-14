1188906f4990727dd17e5ed8e9b8d957564bc9ff
william-reges <william.reges1986@gmail.com>
2025-02-13 21:05:20 -0300

    update

--- dockerfile-push.sh
+++ dockerfile-push.sh
@@ -1,13 +1,23 @@
 VERSION=V3
-NAME_IMAGE=lab-a01-app-service-registry
+NAME_IMAGE=$(cat ./pom.xml | grep "<name>[a-zA-Z0-9]*" | sed "s/<name>//g" | sed "s/<\/name>//g" |  sed -e 's/^[ \t]*//')
 NAME_REPO=williamreges
 
+echo "Versão Atual: ${VERSION}. Informe uma nova versão: "
+read NEW_VERSION
 
-echo "=== Tag Image ===="
-docker tag ${NAME_REPO}/${NAME_IMAGE} ${NAME_REPO}/${NAME_IMAGE}:${VERSION}
+if [ $NEW_VERSION  ]; then
 
-echo "=== PUSH version==="
-docker push ${NAME_REPO}/${NAME_IMAGE}:${VERSION}
+    NEW_VERSION=$(echo $NEW_VERSION | tr "[:lower:]" "[:upper:]");
 
-echo "=== PUSH Latest==="
-docker push ${NAME_REPO}/${NAME_IMAGE}
+    echo "=== Tag Image ===="
+    docker tag ${NAME_REPO}/${NAME_IMAGE} ${NAME_REPO}/${NAME_IMAGE}:${NEW_VERSION}
+
+    echo "=== PUSH version==="
+    docker push ${NAME_REPO}/${NAME_IMAGE}:${NEW_VERSION}
+
+    echo "=== PUSH Latest==="
+    docker push ${NAME_REPO}/${NAME_IMAGE}
+
+    echo "Versão atualizada: ${NEW_VERSION}"
+    sed -i  "s/${VERSION}/${NEW_VERSION}/g"  dockerfile-push.sh
+fi
